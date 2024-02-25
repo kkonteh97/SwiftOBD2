ae29d36f5642d9566be2c9b53531a7ea38dd3218
kkonteh97 <55326260+kkonteh97@users.noreply.github.com>
2024-02-25 10:42:26 -0800

    Initial Commit

---
+++ Tests/SwiftOBD2Tests/SwiftOBD2Tests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SwiftOBD2
+
+final class SwiftOBD2Tests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
